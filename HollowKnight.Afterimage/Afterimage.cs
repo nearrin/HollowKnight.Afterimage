972c551a251000b369760c0b0d1b8d4acc1821cf
Nearrin Yu <nearrin@outlook.com>
2022-09-20 21:27:05 -0700

    U

--- HollowKnight.Afterimage/Afterimage.cs
+++ HollowKnight.Afterimage/Afterimage.cs
@@ -4,37 +4,94 @@ public class Settings
 {
     public bool on = true;
 }
-public class Animation:MonoBehaviour
+public class ImageAnimation : MonoBehaviour
 {
-    public tk2dSpriteAnimationClip clip=null;
+    public tk2dSpriteAnimationClip clip;
+    float time;
     void Update()
     {
-        if (clip != null)
+        var newAnimator = gameObject.GetAddComponent<tk2dSpriteAnimator>();
+        newAnimator.Play(clip);
+        time += Time.deltaTime;
+        if (time > 0.5)
         {
-            var newAnimator = gameObject.GetAddComponent<tk2dSpriteAnimator>();
-            newAnimator.Play(clip);
+            time = 0;
+            gameObject.SetActive(false);
         }
     }
 }
-public class Pool
+public class ImagePool
 {
     public GameObject knightTemplate;
     List<GameObject> activeKnights = new List<GameObject>();
     List<GameObject> inactiveKnights = new List<GameObject>();
-    public GameObject instantiate(Vector3 positon,Quaternion rotation,Vector3 scale)
+    public GameObject instantiate(Vector3 positon, Quaternion rotation, Vector3 scale)
     {
-        var newKnight = UnityEngine.Object.Instantiate(knightTemplate, positon, rotation);
+        List<GameObject> stillActive = new List<GameObject>();
+        foreach (var knight in activeKnights)
+        {
+            if (!knight.activeSelf)
+            {
+                inactiveKnights.Add(knight);
+            }
+            else
+            {
+                stillActive.Add(knight);
+            }
+        }
+        activeKnights = stillActive;
+        GameObject newKnight = null;
+        if (inactiveKnights.Count != 0)
+        {
+            newKnight = inactiveKnights[0];
+            newKnight.SetActive(true);
+            inactiveKnights.RemoveAt(0);
+        }
+        else
+        {
+            newKnight = UnityEngine.Object.Instantiate(knightTemplate);
+            activeKnights.Add(newKnight);
+        }
+        newKnight.transform.position = positon;
+        newKnight.transform.rotation = rotation;
         newKnight.transform.localScale = scale;
         UnityEngine.Object.DontDestroyOnLoad(newKnight);
         newKnight.name = "newKnight";
         return newKnight;
     }
 }
+public class ImageGenerator : MonoBehaviour
+{
+    public ImagePool pool = new();
+    float time;
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 0.1)
+        {
+            time = 0;
+            var originalKnight = HeroController.instance.gameObject;
+            var originalAnimator = originalKnight.GetComponent<tk2dSpriteAnimator>();
+            var newKnight = pool.instantiate(originalKnight.transform.position, originalKnight.transform.rotation, originalKnight.transform.localScale);
+            var newAnimator = newKnight.GetAddComponent<tk2dSpriteAnimator>();
+            newAnimator.SetSprite(originalAnimator.Sprite.Collection, originalAnimator.Sprite.spriteId);
+            newAnimator.Library = originalAnimator.Library;
+            var originalClip = originalAnimator.CurrentClip;
+            var newClip = new tk2dSpriteAnimationClip();
+            newClip.CopyFrom(originalClip);
+            newClip.frames = new tk2dSpriteAnimationFrame[1];
+            newClip.frames[0] = originalClip.frames[originalAnimator.CurrentFrame];
+            newClip.wrapMode = tk2dSpriteAnimationClip.WrapMode.Once;
+            newKnight.GetAddComponent<ImageAnimation>().clip = newClip;
+            newAnimator.enabled = false;
+        }
+    }
+}
 public class Afterimage : Mod, IGlobalSettings<Settings>, IMenuMod
 {
     private Settings settings_ = new();
     public bool ToggleButtonInsideMenu => true;
-    Pool pool = new();
+    ImagePool pool = new();
     public Afterimage() : base("Afterimage")
     {
     }
@@ -46,33 +103,15 @@ public class Afterimage : Mod, IGlobalSettings<Settings>, IMenuMod
                  ("GG_Mighty_Zote","Battle Control"),
             };
     }
-    private void ActiveSceneChanged(UnityEngine.SceneManagement.Scene from, UnityEngine.SceneManagement.Scene to)
-    {
-    }
     private void HeroUpdateHook()
     {
-        if (Input.GetKeyDown(KeyCode.F3))
+        if (HeroController.instance.gameObject.GetComponent<ImageGenerator>() == null)
         {
-            LogDebug("Creating a new instance.");
-            var originalKnight = HeroController.instance.gameObject;
-            var originalAnimator = originalKnight.GetComponent<tk2dSpriteAnimator>();
-            var newKnight = pool.instantiate(originalKnight.transform.position, originalKnight.transform.rotation, originalKnight.transform.localScale);
-            var newAnimator = newKnight.GetAddComponent<tk2dSpriteAnimator>();
-            newAnimator.SetSprite(originalAnimator.Sprite.Collection, originalAnimator.Sprite.spriteId);
-            newAnimator.Library=originalAnimator.Library;
-            var originalClip = originalAnimator.CurrentClip;
-            var newClip = new tk2dSpriteAnimationClip();
-            newClip.CopyFrom(originalClip);
-            newClip.frames = new tk2dSpriteAnimationFrame[1];
-            newClip.frames[0] = originalClip.frames[originalAnimator.CurrentFrame];
-            newClip.wrapMode = tk2dSpriteAnimationClip.WrapMode.Once;
-            newKnight.GetAddComponent<Animation>().clip = newClip;
-            newAnimator.enabled = false;
+            HeroController.instance.gameObject.GetAddComponent<ImageGenerator>().pool = pool;
         }
     }
     public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
     {
-        UnityEngine.SceneManagement.SceneManager.activeSceneChanged += ActiveSceneChanged;
         ModHooks.HeroUpdateHook += HeroUpdateHook;
         var battleControl = preloadedObjects["GG_Mighty_Zote"]["Battle Control"];
         var knightTemplate = battleControl.transform.Find("Zotelings").gameObject.transform.Find("Ordeal Zoteling").gameObject;
@@ -98,8 +137,8 @@ public class Afterimage : Mod, IGlobalSettings<Settings>, IMenuMod
                     Language.Language.Get("MOH_ON", "MainMenu"),
                     Language.Language.Get("MOH_OFF", "MainMenu"),
                 },
-                Saver = i => settings_.on = i==0,
-                Loader = () => settings_.on?0:1
+                Saver = i => settings_.on = i == 0,
+                Loader = () => settings_.on ? 0 : 1
             }
         );
         return menus;
